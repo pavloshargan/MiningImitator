08ac5c5e85602a5bcde5fe455f7e0617654926f6
pavloshargan <pavel.shargan@gmail.com>
2018-03-19 02:28:54 +0200

    New Design
    New Design, fixed some bugs with buttons start/stop. But there are some problems with updating of binding, can't find the right solution.

--- MiningImitator/MainWindow.xaml.cs
+++ MiningImitator/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Threading;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 namespace MiningImitator
 {
@@ -22,14 +23,26 @@ namespace MiningImitator
     /// </summary>
     public class VideoCard
     {
+         
         public string Name { get; set; }
         public int Power { get; set; }
         public Thread thr { get; set; }
         public string State { get; set; }
+         
+        public int CurrentSessionMined { get; set; }
+        
+        public string CurrentCurrency { get; set; }
+         
+        public VideoCard()
+        {
+            CurrentSessionMined = 0;
+        }
+
     }
 
     public partial class MainWindow : Window
     {
+
         public double BTC = 0;
         public double ETH = 0;
         public double KRB = 0;
@@ -40,7 +53,6 @@ namespace MiningImitator
         public void BTCplus(object a)
         {
             VideoCard vc = a as VideoCard;
-            MessageBox.Show(vc.thr.ThreadState.ToString());
             for (; ; )
             {
                 lock (Bobj)
@@ -48,10 +60,13 @@ namespace MiningImitator
                     BTC++;
                     this.Dispatcher.Invoke(() =>
                     {
+
                         BTCLabel.Content = BTC.ToString();
                     }
                     );
                 }
+                vc.CurrentSessionMined++;
+                
                 Thread.Sleep(1000 - vc.Power);
             }
         }
@@ -69,6 +84,7 @@ namespace MiningImitator
                     }
                         );
                 }
+                vc.CurrentSessionMined++;
                 Thread.Sleep(1000 - vc.Power);
             }
         }
@@ -82,52 +98,77 @@ namespace MiningImitator
                     KRB++;
                     this.Dispatcher.Invoke(() =>
                     {
+
                         KRBLabel.Content = KRB.ToString();
                     }
                         );
                 }
+                vc.CurrentSessionMined++;
                 Thread.Sleep(1000 - vc.Power);
             }
         }
         public MainWindow()
         {
             InitializeComponent();
-
+             
             Cards = new ObservableCollection<VideoCard>() { new VideoCard() {
                 Name = "MSI GTX 1060", Power = 600    } ,
                 new VideoCard() { Name = "MSI GTX 1080 Ti", Power = 850  },
-                new VideoCard() { Name = "Gigabyte GTX 1080 Ti", Power=1000 },
+                new VideoCard() { Name = "Gigabyte GTX 1080 Ti", Power = 950 },
                 new VideoCard() { Name = "INNO3D GTX 1080 Ti", Power = 800 },
                 new VideoCard() { Name = "Asus PCI-Ex GeForce GT", Power = 250  }
             };
+
             MyList.ItemsSource = Cards;
+
         }
 
         private void Start_Click(object sender, RoutedEventArgs e)
         {
-            VideoCard card = MyList.SelectedItem as VideoCard;
 
-            switch ((CardCurrency.SelectedItem as ComboBoxItem).Content.ToString())
+
+            string ChoosenCurrency = (CardCurrency.SelectedItem as ComboBoxItem).Content.ToString();
+            VideoCard card = MyList.SelectedItem as VideoCard;
+            if (card.CurrentCurrency == ChoosenCurrency)
             {
-                case "BTC": { card.thr = new Thread(BTCplus); break; }
-                case "KRB": { card.thr = new Thread(KRBplus); break; }
-                case "ETH": { card.thr = new Thread(ETHplus); break; }
-                default: { return; }
+                card.thr.Resume();
             }
-            card.thr.Start(card);
+            else
+            {
+                switch (ChoosenCurrency)
+                {
+                    case "BTC": { card.thr = new Thread(BTCplus); card.CurrentCurrency = "BTC";  break; }
+                    case "KRB": { card.thr = new Thread(KRBplus); card.CurrentCurrency = "KRB"; break; }
+                    case "ETH": { card.thr = new Thread(ETHplus); card.CurrentCurrency = "ETH"; break; }
+                    default: { return; }
+                }
+                
+                card.CurrentSessionMined = 0;
+                card.thr.Start(card);
+            }
+
+
             card.State = "Running";
             Start.IsEnabled = false;
             Stop.IsEnabled = true;
+            MyList.Items.Refresh();
+            
         }
 
         private void Stop_Click(object sender, RoutedEventArgs e)
         {
+
             VideoCard card = MyList.SelectedValue as VideoCard;
-            card.thr.Abort();
+            card.thr.Suspend();
             card.State = "Suspended";
+
             Start.IsEnabled = true;
             Stop.IsEnabled = false;
+            MyList.Items.Refresh();
         }
+
+
+
         private void Window_Closed(object sender, EventArgs e)
         {
             foreach (VideoCard v in Cards)
@@ -137,8 +178,10 @@ namespace MiningImitator
             }
             Application.Current.Shutdown();
         }
+
         private void MyList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+
             VideoCard vc = MyList.SelectedValue as VideoCard;
             if (vc.thr == null)
             {
@@ -146,17 +189,23 @@ namespace MiningImitator
                 Stop.IsEnabled = false;
                 return;
             }
-            MessageBox.Show(vc.State);
+
             if (vc.State == "Running")
             {
                 Start.IsEnabled = false;
                 Stop.IsEnabled = true;
             }
-            if (vc.thr.ThreadState.ToString() == "Suspended")
+            if (vc.State == "Suspended")
             {
                 Start.IsEnabled = true;
                 Stop.IsEnabled = false;
             }
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window_Closed(sender,e);
+        }
     }
 }
+
