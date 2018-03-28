068f2c1f54a8ac43f329c55759228f3c7e1f1bcc
pavloshargan <pavel.shargan@gmail.com>
2018-03-28 22:41:48 +0300

    Binding has been completed

--- MiningImitator/MainWindow.xaml.cs
+++ MiningImitator/MainWindow.xaml.cs
@@ -15,31 +15,48 @@ using System.Windows.Shapes;
 using System.Threading;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Globalization;
 
 namespace MiningImitator
 {
+    
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public class VideoCard
+    public class VideoCard : INotifyPropertyChanged
     {
          
         public string Name { get; set; }
         public int Power { get; set; }
         public Thread thr { get; set; }
         public string State { get; set; }
-         
-        public int CurrentSessionMined { get; set; }
-        
+         public int current_session_mined;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public int CurrentSessionMined
+        {
+            get
+            {
+                return current_session_mined;
+            }
+            set
+            {
+                current_session_mined = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("CurrentSessionMined"));
+            }
+        }
+
         public string CurrentCurrency { get; set; }
          
         public VideoCard()
         {
-            CurrentSessionMined = 0;
+            current_session_mined = 0;
         }
-
+       
+      
     }
 
+   
     public partial class MainWindow : Window
     {
 
@@ -50,6 +67,23 @@ namespace MiningImitator
         public object Eobj = new object();
         public object Kobj = new object();
         public ObservableCollection<VideoCard> Cards { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Cards = new ObservableCollection<VideoCard>() { new VideoCard() {
+                Name = "MSI GTX 1060", Power = 600  } ,
+                new VideoCard() { Name = "MSI GTX 1080 Ti", Power = 850  },
+                new VideoCard() { Name = "Gigabyte GTX 1080 Ti", Power = 950 },
+                new VideoCard() { Name = "INNO3D GTX 1080 Ti", Power = 800 },
+                new VideoCard() { Name = "Asus PCI-Ex GeForce GT", Power = 250  }
+            };
+
+            MyList.ItemsSource = Cards;
+
+        }
+
         public void BTCplus(object a)
         {
             VideoCard vc = a as VideoCard;
@@ -107,25 +141,10 @@ namespace MiningImitator
                 Thread.Sleep(1000 - vc.Power);
             }
         }
-        public MainWindow()
-        {
-            InitializeComponent();
-             
-            Cards = new ObservableCollection<VideoCard>() { new VideoCard() {
-                Name = "MSI GTX 1060", Power = 600    } ,
-                new VideoCard() { Name = "MSI GTX 1080 Ti", Power = 850  },
-                new VideoCard() { Name = "Gigabyte GTX 1080 Ti", Power = 950 },
-                new VideoCard() { Name = "INNO3D GTX 1080 Ti", Power = 800 },
-                new VideoCard() { Name = "Asus PCI-Ex GeForce GT", Power = 250  }
-            };
-
-            MyList.ItemsSource = Cards;
-
-        }
+       
 
         private void Start_Click(object sender, RoutedEventArgs e)
         {
-
 
             string ChoosenCurrency = (CardCurrency.SelectedItem as ComboBoxItem).Content.ToString();
             VideoCard card = MyList.SelectedItem as VideoCard;
@@ -152,7 +171,11 @@ namespace MiningImitator
             Start.IsEnabled = false;
             Stop.IsEnabled = true;
             MyList.Items.Refresh();
-            
+
+            MineInfo.Visibility = Visibility.Visible;
+            MinedRun.SetBinding(Run.TextProperty, new Binding { Source = MyList.SelectedItem, Path = new PropertyPath("CurrentSessionMined"), Mode = BindingMode.TwoWay });
+            CurrencyRun.SetBinding(Run.TextProperty, new Binding { Source = MyList.SelectedItem, Path = new PropertyPath("CurrentCurrency"), Mode = BindingMode.TwoWay });
+
         }
 
         private void Stop_Click(object sender, RoutedEventArgs e)
@@ -184,6 +207,8 @@ namespace MiningImitator
 
         private void MyList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            MinedRun.SetBinding(Run.TextProperty, new Binding { Source = MyList.SelectedItem, Path = new PropertyPath("CurrentSessionMined"), Mode = BindingMode.TwoWay });
+            CurrencyRun.SetBinding(Run.TextProperty, new Binding { Source = MyList.SelectedItem, Path = new PropertyPath("CurrentCurrency"), Mode = BindingMode.TwoWay });
 
             VideoCard vc = MyList.SelectedValue as VideoCard;
             if (vc.thr == null)
@@ -192,7 +217,7 @@ namespace MiningImitator
                 Stop.IsEnabled = false;
                 return;
             }
-
+            
             if (vc.State == "Running")
             {
                 Start.IsEnabled = false;
